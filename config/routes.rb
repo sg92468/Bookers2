c31960686def1a9a0fb0dabe811e0c1ea2e17d8a
EC2 Default User <ec2-user@ip-172-31-13-22.ec2.internal>
2021-06-02 01:41:04 +0000

    Start favorites function

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
