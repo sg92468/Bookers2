354b17fc7bbcda74ea9f1a771c6b521bfaf171ab
EC2 Default User <ec2-user@ip-172-31-13-22.ec2.internal>
2021-06-02 03:52:35 +0000

    Finished add favorite function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
  
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
