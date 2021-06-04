4eef8699f70c783d5bda0439bfec1394834de17b
EC2 Default User <ec2-user@ip-172-31-13-22.ec2.internal>
2021-06-04 03:02:22 +0000

    [Finished] Add FollowFunction

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+    
+  resources :relationships, only: %I[create destroy]
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
